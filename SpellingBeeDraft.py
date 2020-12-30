9ddb3a5d568ff5422a1795d00b7561b82c247142
Nicholas Kohut <nicholaskohut@Nicholass-MacBook-Air-2.local>
2020-12-29 20:57:47 -0800

    updating my solution to look like Kevin's

--- SpellingBeeDraft.py
+++ SpellingBeeDraft.py
@@ -4,27 +4,12 @@ from english_words import english_words_lower_alpha_set as all_words
 def SpellingBee(letters, key_letter):
     #Empty array to hold all the solutions
     solutions = []
-
-    #Dictionary of all the words
-    #dictionary = open("/usr/share/dict/words", 'r')
-
+    letters = frozenset(letters)
     ## Loop through the dictionary
     for word in all_words:
-
     ## Make sure word is long enough and the key letter is in the word
-        if len(word) >= 4 and key_letter in word:
-        #Make sure each letter of the word is in the set "letters"
-            # Assume the word is good
-            letter_flag = True
-            for letter in word:
-                if letter not in letters:
-                    # The word is not good
-                    letter_flag = False
-            #After looping through every letter, check flag
-            if letter_flag:
-                ## Append to the solution set
-                solutions.append(word)
-
+        if len(word) >= 4 and key_letter in word and letters >= set(word):
+            solutions.append(word)
     ## Return the solution set
     return(solutions)
 
